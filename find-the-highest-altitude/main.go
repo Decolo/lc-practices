5d3f4e9be51010aa8ab2e48d2b934c7190c33e03
Decolo <caicheng.fe.dev@gmail.com>
2025-10-19 20:58:17 +0800

    feat: golang version of find-the-highest-altitude

---
+++ find-the-highest-altitude/main.go
@@ -0,0 +1,13 @@
+package main
+
+func largestAltitude(gain []int) int {
+	maxPoint := 0
+	sum := 0
+
+	for i := 0; i < len(gain); i++ {
+		sum += gain[i]
+		maxPoint = max(maxPoint, sum)
+	}
+
+	return maxPoint
+}
